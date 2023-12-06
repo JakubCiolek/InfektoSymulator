1e90c3fd8ac4ec441dc07cbd047a74f3e53b64a3
JakubCiolek <kubaciolek383@gmail.com>
2023-12-06 23:36:13 +0100

    Reading Values from sliders

--- InfektoSymulator/Assets/Scripts/interfaceScript.cs
+++ InfektoSymulator/Assets/Scripts/interfaceScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,23 +10,51 @@ public class interfaceScritp : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public Slider population;
+    public GameObject population;
+    public GameObject procentInfected;
+    public GameObject populationImmunity;
+    public GameObject virusSpreadFactor;
+    public GameObject distanceToExpose;
+    public GameObject timeToExpose;
+    public GameObject simDuration;
+    public GameObject simSpeed;
     void Start()
     {
-        
+        population.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (population);});
+        procentInfected.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (procentInfected);});
+        populationImmunity.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (populationImmunity);});
+        virusSpreadFactor.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (virusSpreadFactor);});
+        distanceToExpose.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (distanceToExpose);});
+        timeToExpose.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (timeToExpose);});
+        simDuration.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (simDuration);});
+        simSpeed.GetComponentInChildren<Slider>().onValueChanged.AddListener (delegate {updateValueHints (simSpeed);});
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public Dictionary<String, float> GetSimulationParameters()
     {
         var simParameters = new Dictionary<String, float>();
-        simParameters["population"] = population.value;
+        simParameters["population"] = population.GetComponentInChildren<Slider>().value;
+        simParameters["procentInfected"] = procentInfected.GetComponentInChildren<Slider>().value;
+        simParameters["populationImmunity"] = populationImmunity.GetComponentInChildren<Slider>().value;
+        simParameters["virusSpreadFactor"] = virusSpreadFactor.GetComponentInChildren<Slider>().value;
+        simParameters["distanceToExpose"] = distanceToExpose.GetComponentInChildren<Slider>().value;
+        simParameters["timeToExpose"] = timeToExpose.GetComponentInChildren<Slider>().value;
+        simParameters["simulationDuration"] = simDuration.GetComponentInChildren<Slider>().value;
+        simParameters["simulationSpeed"] = simSpeed.GetComponentInChildren<Slider>().value;
         //TODO : read parameters and put them in dictionary.
         return simParameters;
     }
+
+    private void updateValueHints(GameObject sliderObject)
+    {
+        var slider = sliderObject.GetComponentInChildren<Slider>();
+        var textBox = sliderObject.GetComponentInChildren<TMP_Text>();
+        textBox.text = slider.value.ToString("F" + 2);
+    }
+
 }
